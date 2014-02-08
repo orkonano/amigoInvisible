1f6ceaee33c63d0b708b593eadddc193eb2cc847
orko <orquito@gmail.com>
2014-02-08 18:20:50 -0300

    Fix bug que hacia que no funcione el mensaje, ya que estaba incluyendo un prototype en un singleton

--- orko/src/groovy/orko/dev/amigoInvisible/transform/EmailTransformer.groovy
+++ orko/src/groovy/orko/dev/amigoInvisible/transform/EmailTransformer.groovy
@@ -4,28 +4,27 @@ package orko.dev.amigoInvisible.transform
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.MailMessage
+import org.springframework.mail.SimpleMailMessage
 import org.springframework.stereotype.Component
 
 import orko.dev.amigoInvisible.domain.Regalo
 
-@Component
-class EmailTransformer {
+abstract class EmailTransformer {
 
-	MailMessage mailMessage;
-
-	@Autowired
-	EmailTransformer(MailMessage mailMessage){
-		this.mailMessage = mailMessage
+	EmailTransformer(){
 	}
 	  
 	public MailMessage transform(Regalo recipient) {
 		if (recipient == null) {
 			return null;
 		}
-		mailMessage.setTo(recipient.amigoFrom.email);
+        def mailMessage = createSimpleMailMessage()
+        mailMessage.setTo(recipient.amigoFrom.email);
 		mailMessage.setSubject("${recipient.amigoFrom.nombre} tu amigo invisible es....")
 		mailMessage.setText(".....${recipient.amigoTo.nombre}");
   
 	   return mailMessage;
 	}
+
+    public abstract SimpleMailMessage createSimpleMailMessage()
 }
